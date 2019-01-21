5171d7c2b0af27031273458865ee5fc37d29075f
joachimprinzbach <joachim.prinzbach@gmail.com>
2019-01-21 14:05:21 +0100

    Improve profiles

--- src/main/java/ch/springboot/fundamentals/pizzaapp/infrastructure/RemoteCatalogService.java
+++ src/main/java/ch/springboot/fundamentals/pizzaapp/infrastructure/RemoteCatalogService.java
@@ -1,5 +1,6 @@
 package ch.springboot.fundamentals.pizzaapp.infrastructure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,13 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Profile("all")
+@Profile("!mock")
 public class RemoteCatalogService implements CatalogService {
+
+    @Value("${pizzaservice.url}")
+    private String pizzaServiceUrl;
+
+    @Value("${pizzaservice.port}")
+    private String pizzaServicePort;
 
     @Override
     public boolean isPizzaAvailable(String name) {
         RestTemplate restTemplate = new RestTemplate();
-        CatalogEntryDto catalogEntryDto = restTemplate.getForObject("http://localhost:8090/api/catalog/" + name, CatalogEntryDto.class);
+        CatalogEntryDto catalogEntryDto = restTemplate.getForObject(getCatalogBaseApiUrl()+ "/" + name, CatalogEntryDto.class);
         System.out.println("real");
         return catalogEntryDto.isAvailable();
     }
@@ -22,8 +29,12 @@ public class RemoteCatalogService implements CatalogService {
     @Override
     public List<CatalogEntryDto> getPizzaInventory() {
         RestTemplate restTemplate = new RestTemplate();
-        List<CatalogEntryDto> catalog = restTemplate.getForObject("http://localhost:8090/api/catalog/", ArrayList.class);
+        List<CatalogEntryDto> catalog = restTemplate.getForObject(getCatalogBaseApiUrl(), ArrayList.class);
         System.out.println("real");
         return catalog;
+    }
+
+    private String getCatalogBaseApiUrl() {
+        return pizzaServiceUrl + ":" + pizzaServicePort + "/api/catalog";
     }
 }
